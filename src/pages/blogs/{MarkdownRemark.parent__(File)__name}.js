164bd3c097259dbec0b4ff11a731d727818aed21
Guido <guidocalciano@gmail.com>
2022-11-03 21:02:10 -0300

    minor updates

--- src/pages/blogs/{MarkdownRemark.parent__(File)__name}.js
+++ src/pages/blogs/{MarkdownRemark.parent__(File)__name}.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../../components/layout';
 import * as classes from '../../styles/blog.module.css'
 
-const BlogPostPage = () => {
+const BlogPostPage = (props) => {
     return (
         <Layout>
             <div>
